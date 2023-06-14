a403f68ccba54a57c5e03f4bdc87085d225bb4ed
sekvi99 <filmatkozlik@gmail.com>
2023-06-14 10:53:08 +0200

    Adding propper endpoint definition

--- react-flask-app/api/python-backend/api.py
+++ react-flask-app/api/python-backend/api.py
@@ -1,9 +1,36 @@
-import time
-
 from flask import Flask
+from consts import *
+from python_api_data.api_calls.endpoint_api_calls.currency_rates_request import CurrencyRatesRequestManager
+from python_api_data.api_calls.endpoint_api_calls.stock_exchange_request import StockMarketAPIRequestManager
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/getCurrencyData/<currency>', methods=['GET'])
+def refresh_currency_rates(currency: str) -> dict:
+    """
+    Declaration of endpoint for fetching currency rates and return them in propper .json format.
+
+    Args:
+        currency (str): String representation of currency to api endpoint.
+    """
+    cr = CurrencyRatesRequestManager(
+        validate_regex=URL_REGEX,
+        request_type='GET',
+        endpoint=CURRENCY_RATES_URL,   
+    )
+    return cr.process_api_response(currency)
+
+@app.route('/getStockMarketData/<symbol>', methods=['GET'])
+def refresh_stock_market_data(symbol: str) -> dict:
+    """
+    Declaration of endpoint for fetching stock market data and return them in propper .json format.
+
+    Args:
+        symbol (str): String representation of currency to api endpoint.
+    """
+    sm = StockMarketAPIRequestManager(
+        validate_regex=URL_REGEX,
+        request_type='GET',
+        endpoint=STOCK_EXCHANGE_URL
+    )
+    return sm.process_api_response(symbol)
