5740fb8ec0b1591a1292db48177466b47de080cb
sekvi99 <filmatkozlik@gmail.com>
2023-06-21 17:02:42 +0200

    Creating layout component

---
+++ react-flask-app/client/src/components/Layout/layout.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import Sidebar from '../Sidebar/sidebar';
+import Navbar from '../Navbar/navbar';
+import Footer from '../Footer/footer';
+import { ILayout } from '../../interfaces/layout.interface';
+import { User } from '../../types';
+import httpClient from '../../httpClient';
+
+const Layout: React.FC<ILayout> = ( { children }) => {
+   
+    const [isOpen, setIsOpen] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
+
+    const toggle = (): void => {
+        setIsOpen(!isOpen);
+    }
+
+    useEffect(() => {
+        (async () => {
+          try {
+            const resp = await httpClient.get("//localhost:5000/@me");
+            setUser(resp.data);
+          } catch (error) {
+            console.log("Not authenticated");
+          }
+        })();
+      }, []);
+
+    return (
+      <div>
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        <Navbar toggle={toggle} user={user}/>
+        {children}
+        <Footer />
+      </div>
+    )
+}
+
+export default Layout;
